394a4c98b743783e9ebce04c33d144cc1a8c4aba
Travis M. Moore <travis.moore.phd@gmail.com>
2022-07-27 10:49:35 -0500

    Cleaned up code a little.

--- tree.py
+++ tree.py
@@ -1,3 +1,11 @@
+""" Recruiting Tool:
+    GUI for browsing subjects from the 'database'
+
+    Written by: Travis M. Moore
+    Created: Jul 26, 2022
+"""
+
+# Import GUI packages
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo
@@ -27,25 +35,20 @@ class App(tk.Tk):
         # Load in database
         self.db = m.SubDB()
 
+        # Set up main window
         self.create_tree_widget()
         self.create_main_frame()
-        #self.create_audiogram()
+
+
+    def create_tree_widget(self):
+        self.sub_tree = v.SubjectTree(self, self.db)
+        self.sub_tree.grid(row=0, column=0, sticky='ns')
+        self.sub_tree.bind('<<TreeviewSelect>>', self._item_selected)
 
 
     def create_main_frame(self):
         self.main_frame = v.MainFrame(self)
         self.main_frame.grid(row=0, column=2)
-
-    def create_audiogram(self):
-        self.audiogram = v.MPLPlot()
-        plot_area = v.PlotFrame(self)
-        plot_area.add_mpl_figure(self.audiogram)
-
-    
-    def create_tree_widget(self):
-        self.sub_tree = v.SubjectTree(self, self.db)
-        self.sub_tree.grid(row=0, column=0, sticky='ns')
-        self.sub_tree.bind('<<TreeviewSelect>>', self._item_selected)
 
 
     def _item_selected(self, *_):
@@ -57,10 +60,13 @@ class App(tk.Tk):
             self.main_frame._vars['smartphone_type'].set(self.db.data[self.db.data['Subject Id'] == record]['Smartphone Type'].values[0])
             self.main_frame._vars['will_not_wear'].set(self.db.data[self.db.data['Subject Id'] == record]['Will Not Wear'].values[0])
 
-            # Study dates and names
+            # Study dates and names parsing
             latest_study = self.db.data[self.db.data['Subject Id'] == record]['Latest Study'].values[0]
             study_dates = [z.split(')')[0] for z in latest_study.split('(') if ')' in z]
-            study_dates = study_dates[0]
+            try:
+                study_dates = study_dates[0]
+            except:
+                study_dates = '-'
             study_info = latest_study.split('(')[0]
             self.main_frame._vars['study_dates'].set(study_dates)
             self.main_frame._vars['study_info'].set(study_info)
@@ -73,15 +79,20 @@ class App(tk.Tk):
             self.main_frame._vars['r_receiver'].set(self.db.data[self.db.data['Subject Id'] == record]['Right Ric Cable Size'].values[0])
             self.main_frame._vars['l_receiver'].set(self.db.data[self.db.data['Subject Id'] == record]['Left Ric Cable Size'].values[0])
 
+            # Call audio display function
+            self._show_audio()
 
-            figure = Figure(figsize=(5, 5), dpi=100)
-            figure_canvas = FigureCanvasTkAgg(figure, self)
-            #NavigationToolbar2Tk(figure_canvas, self)
-            axes = figure.add_subplot()
-            xs = [random.randint(10,20) for i in range(0,4)]
-            axes.plot(xs, [5,6,7,8])
-            #db.audio_ac(str(record))
-            figure_canvas.get_tk_widget().grid(row=10, column=2)#(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+    def _show_audio(self):
+        """ Plot audiogram for selected subject """
+        figure = Figure(figsize=(5, 5), dpi=100)
+        figure_canvas = FigureCanvasTkAgg(figure, self)
+        #NavigationToolbar2Tk(figure_canvas, self)
+        axes = figure.add_subplot()
+        xs = [random.randint(10,20) for i in range(0,4)]
+        axes.plot(xs, [5,6,7,8])
+        #db.audio_ac(str(record))
+        figure_canvas.get_tk_widget().grid(row=10, column=2)#(side=tk.TOP, fill=tk.BOTH, expand=True)
 
 
 if __name__ == '__main__':
--- views.py
+++ views.py
@@ -61,7 +61,6 @@ class MainFrame(ttk.Frame):
         ttk.Label(lfrm_data, text="Will Not Wear:").grid(row=5, column=0, sticky='e')
         ttk.Label(lfrm_data, textvariable=self._vars['will_not_wear']).grid(row=5, column=1, sticky='w')
 
-
         # LEFT SIDE
         lfrm_left = ttk.LabelFrame(self, text="Left Side")
         lfrm_left.grid(row=1, column=0, **options, sticky='e')
@@ -74,7 +73,6 @@ class MainFrame(ttk.Frame):
         # Receiver length
         ttk.Label(lfrm_left, text="Receiver Length:").grid(row=2, column=0, sticky='e')
         ttk.Label(lfrm_left, textvariable=self._vars['l_receiver']).grid(row=2, column=1, sticky='w')
-
 
         # RIGHT SIDE
         lfrm_right = ttk.LabelFrame(self, text="Right Side")
@@ -90,25 +88,22 @@ class MainFrame(ttk.Frame):
 
 
 class SubjectTree(tk.Frame):
-    """ Display for database subjects """
+    """ Treeview for database subjects """
     def __init__(self, parent, db, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
         
+        # Initialize
         self.db = db
-        #self.item_selected = item_selected
+        #self.columnconfigure(0, weight=1)
+        #self.rowconfigure(0, weight=1)
 
-        self.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
-
+        # Tree
+        # Get subs from dataframe
+        subjects = self.db.data['Subject Id']
         columns = ('subject_id')
-
         self.tree = ttk.Treeview(self, columns=columns, show='headings', height=20)
-
-        # Tree headings
         self.tree.heading('subject_id', text='Subject ID')
-
         self.tree.grid(row=0, column=0, sticky='nsew')
-
         self.tree.bind('<<TreeviewSelect>>', self._item_selected)
 
         # Scrollbar
@@ -116,39 +111,11 @@ class SubjectTree(tk.Frame):
         self.tree.configure(yscroll=self.scrollbar.set)
         self.scrollbar.grid(row=0, rowspan=10, column=1, sticky='ns')
 
-        # Get subs from dataframe
-        contacts = self.db.data['Subject Id']
+        # Add subjects to tree
+        for subject in subjects:
+            self.tree.insert('', tk.END, values=subject)
 
-        # Add df data to tree
-        for contact in contacts:
-            self.tree.insert('', tk.END, values=contact)
 
     def _item_selected(self, *args):
-        global row_id
-        print("Item selected and View responded!")
+        """ Trigger event that tree item was selected """
         self.event_generate('<<TreeviewSelect>>')
-        row_id = self.tree.focus()
-        print(row_id)
-
-
-# Audiogram
-class PlotFrame(tk.Frame):
-    def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
-        
-    def add_mpl_figure(self, fig):
-        self.mpl_canvas = FigureCanvasTkAgg(fig, self)
-        self.mpl_canvas.draw()
-        self.mpl_canvas.get_tk_widget().grid(row=9, column=2)#pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-
-        #self.toolbar = NavigationToolbar2Tk(self.mpl_canvas, self)
-        #self.toolbar.update()
-        #self.mpl_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-
-
-class MPLPlot(Figure):
-    def __init__(self):
-        Figure.__init__(self, figsize=(5,5), dpi=100)
-        self.plot = self.add_subplot(111)
-        self.plot.plot([1,2,3,4,5], [6,7,8,9,0])
-
